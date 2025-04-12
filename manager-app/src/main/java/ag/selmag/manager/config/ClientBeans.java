c006576eb7b4628f6c81490326b6bc14e8e6142b
NickitaB <nickitaB@gmail.com>
2025-04-12 13:32:50 +0300

    spring boot standard security without user generation

--- manager-app/src/main/java/ag/selmag/manager/config/ClientBeans.java
+++ manager-app/src/main/java/ag/selmag/manager/config/ClientBeans.java
@@ -4,6 +4,7 @@ import ag.selmag.manager.controller.client.RestClientProductsRestClientImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -11,9 +12,12 @@ public class ClientBeans {
 
   @Bean
   public RestClientProductsRestClientImpl productsRestClient(
-          @Value("${selmag.services.uri:http://localhost:8081}") String catalogueBaseUri) {
+          @Value("${selmag.services.uri:http://localhost:8081}") String catalogueBaseUri,
+          @Value("${selmag.services.username:}") String catalogueUsername,
+          @Value("${selmag.services.password:}") String cataloguePassword) {
     return new RestClientProductsRestClientImpl(RestClient.builder()
             .baseUrl(catalogueBaseUri)
+            .requestInterceptor(new BasicAuthenticationInterceptor(catalogueUsername,cataloguePassword))
             .build());
   }
 }
